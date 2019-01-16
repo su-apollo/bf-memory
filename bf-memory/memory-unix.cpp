a5737d8e1016eddd588a6bf7e2aa1321ca2be54c
apollohpp <apollo.hpp@gmail.com>
2019-01-16 19:26:58 +0900

    Modify memory-unix.cpp to use posix_memalign

--- bf-memory/memory-unix.cpp
+++ bf-memory/memory-unix.cpp
@@ -1,14 +1,23 @@
 #include "memory.hpp"
 
 #if BF_UNIX_FAMILY
+#include <new>
 
 namespace bf {
 void* aligned_alloc(std::size_t size) {
-	return ::aligned_alloc(BF_MEMORY_ALLOCATION_ALIGNMENT, size);
+	void *p = nullptr;
+	if (posix_memalign(&p, BF_MEMORY_ALLOCATION_ALIGNMENT, size))
+		throw std::bad_alloc();
+
+	return p;
 }
 
 void* aligned_alloc(std::size_t alignment, std::size_t size) {
-	return ::aligned_alloc(alignment, size);
+	void *p = nullptr;
+	if (posix_memalign(&p, alignment, size))
+		throw std::bad_alloc();
+
+	return p;
 }
 
 void free(void* ptr) {
