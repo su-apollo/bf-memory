d3b5c59d5f145d3c4d573f7a40cdc0be092f4c0c
apollohpp <apollo.hpp@gmail.com>
2019-01-18 17:54:21 +0900

    Modify main.cpp to test std 'thead' & ''future'

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,6 +1,10 @@
 #include "allocator.hpp"
 
 #include <iostream>
+#include <thread>
+#include <future>
+#include <numeric>
+#include <random>
 
 #if BF_WINDOWS_FAMILY
 
@@ -53,11 +57,7 @@ struct node : bf::atomic::node {
 	int value;
 };
 
-int main() {
-	std::cout << "Hello World!" << std::endl;
-
-	bf::pool = new bf::memory_pool;
-
+void test_stack() {
 	std::cout << "malloc_info : " << alignof(bf::malloc_info) << std::endl;
 
 	bf::atomic::stack s;
@@ -77,19 +77,100 @@ int main() {
 		if (head != nullptr)
 			std::cout << "head : " << head->value << std::endl;
 	} while (head != nullptr);
+}
 
-	bf::vector<int> test;
+void accumulate(bf::vector<int>::iterator first, bf::vector<int>::iterator last, std::promise<int> accumulate_promise) {
 
-	test.push_back(1);
-	test.push_back(2);
-	test.push_back(3);
+	std::cout << "promise ref " << &accumulate_promise << std::endl;
 
-	for (auto i : test) {
-		std::cout << "test" << std::endl;
-	}
+	int sum = std::accumulate(first, last, 0);
+	accumulate_promise.set_value(sum);
+}
+
+void test_promise() {
+	bf::vector<int> numbers = { 1, 2, 3, 4, 5, 6};
+	std::promise<int> accumulate_promise;
+	std::future<int> accumulate_future = accumulate_promise.get_future();
+
+	std::cout << "promise ref " << &accumulate_promise << std::endl;
+
+	std::thread work(accumulate, numbers.begin(), numbers.end(), std::move(accumulate_promise));
+
+    accumulate_future.wait();
+    std::cout << "acccumulate result " << accumulate_future.get() << std::endl;
+	work.join();
+}
+
+struct test {
+	test(int i) : value(i) {};
+	~test() = default;
+
+	int value;
+};
+
+void test_thread_safe() {
+	std::packaged_task<int()> task([]{ 
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(1, 100);
+
+		for (auto i = 0 ; i < 10000 ; ++i) {
+			std::cout << "call task" << std::endl;
+			auto p = bf::make_unique<test>(dis(gen));
+			std::cout << "value " << p->value << std::endl;
+		}
+		return 7; 
+	});
+    std::future<int> f1 = task.get_future();
+    std::thread t(std::move(task));
+ 
+    std::future<int> f2 = std::async(std::launch::async, []{ 
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(1, 100);
+
+		for (auto i = 0 ; i < 10000 ; ++i) {
+			std::cout << "call async" << std::endl;
+			auto p = bf::make_unique<test>(dis(gen));
+			std::cout << "value " << p->value << std::endl;
+		}
+		return 8; 
+	});
+ 
+    std::promise<int> p;
+    std::future<int> f3 = p.get_future();
+    std::thread( [&p]{ 
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(1, 100);
+
+		for (auto i = 0 ; i < 10000 ; ++i) {
+			std::cout << "call thread" << std::endl;
+			auto p = bf::make_unique<test>(dis(gen));
+			std::cout << "value " << p->value << std::endl;
+		}
+		p.set_value_at_thread_exit(9); 
+	}).detach();
+ 
+    std::cout << "Waiting..." << std::flush;
+    f1.wait();
+    f2.wait();
+	f3.wait();
+
+    std::cout << "Done!\nResults are: "
+              << f1.get() << ' ' << f2.get() << ' ' << f3.get() << '\n';
+    t.join();
+}
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	bf::pool = new bf::memory_pool;
+
+	test_stack();
+	test_promise();
+	test_thread_safe();
 
 	delete bf::pool;
-
 	return 0;	
 }
 
